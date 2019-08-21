9e746c2e2fd4646b04e509e1aea5c5dcda45afd7
Wilfred Alec Vernon <wilf.vernon@gmail.com>
2019-08-21 02:29:09 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,11 +1,15 @@
 def find_element_index(array, value_to_find)
-  # Add your solution here
+ array.index(value_to_find)
 end
+
+
 
 def find_max_value(array)
-  # Add your solution here
+  newarray = array.sort
+  newarray[-1]
 end
 
-def find_min_value(array)
-  # Add your solution here
+def find_min_value(array) 
+  newarray = array.sort
+  newarray[0]
 end
