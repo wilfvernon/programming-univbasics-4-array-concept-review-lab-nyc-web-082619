4046ef799acd6942dc0551b22e40b0c8530c9e9a
Wilfred Alec Vernon <wilf.vernon@gmail.com>
2019-08-21 02:02:40 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,6 +1,13 @@
 def find_element_index(array, value_to_find)
-  # Add your solution here
+  newarray = array
+  counter = 0
+  unless newarray[0] == value_to_find do
+    newarray.shift
+    counter += 1
+  end
+  p counter
 end
+
 
 def find_max_value(array)
   # Add your solution here
